eb17f03d4e6f5fa2a1c7348b7762d5f5846c05ad
benbeckman2001 <trungthekop123@gmail.com>
2024-02-16 14:59:46 +0700

    Test push branch main2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController, testController } from './app.controller';
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { HumansModule } from './humans/human.module';
 
 @Module({
   imports: [CatsModule, HumansModule],
-  controllers: [AppController, testController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
