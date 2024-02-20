3a60994627c1dd9a59c5263bb83cbca5334b7fa0
Trunks2k1ynwa <trungbts3@gmail.com>
2024-02-20 21:57:53 +0700

    Update module connect database by ORM

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,8 +1,11 @@
-import { Global, Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { CatsController } from './cat.controller';
 import { CatsService } from './cat.service';
+import { CommonModule } from 'src/common/common.module';
+import { ConfigModule } from '@nestjs/config';
 // @Global()
 @Module({
+  imports: [forwardRef(() => CommonModule), ConfigModule],
   controllers: [CatsController],
   providers: [CatsService],
   exports: [CatsService],
