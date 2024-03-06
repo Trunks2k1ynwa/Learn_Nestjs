63d7f889a45bbe348172e6f546be56ca323233a1
Darwin.Le <darwin.le@avepoint.com>
2024-03-06 10:03:05 +0700

    Auto validation by Dto

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateAccountDto } from './dto/createAccount.dto';
 import { AccountService } from './account.service';
 
@@ -6,8 +14,14 @@ import { AccountService } from './account.service';
 export class AccountController {
   constructor(private accountService: AccountService) {}
   @Post('create-account')
+  @UsePipes(new ValidationPipe({ transform: true }))
   createAccount(@Body() createAccount: CreateAccountDto) {
-    console.log('🚀 ~ createAccount:', createAccount);
     return this.accountService.createAccount(createAccount);
+  }
+  @UsePipes(new ValidationPipe({ transform: true }))
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    console.log(typeof id === 'number'); // true
+    return 'This action returns a user';
   }
 }
